cf07723bcb41d5ff92eea06dd0a1e01cf6d1aabe
Somchai <64143186@g.cmru.ac.th>
2024-07-07 16:48:52 +0700

    http

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomeScreen(),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: HomeScreen());
   }
 }
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,9 +10,39 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final String API_URL = 'http://127.0.0.1:5001/todos';
   List todoList = [];
 
+  Future<List> fetchtodoList() async {
+    final response = await http.get(Uri.parse(API_URL));
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to load');
+    }
+  }
+
+  @override
+  void initState() {
+    fetchtodoList();
+    // TODO: implement initState
+    super.initState();
+    fetchtodoList().then((value) {
+      setState(() {
+        todoList = value;
+      });
+    });
+  }
+
   final task = TextEditingController();
+
+  Future<void> createTodo() async {
+    final response = await http.post(
+      Uri.parse(API_URL),
+      headers: {'Content-Type': 'application/json'},
+      body: {Title: task.text},
+    );
+  }
 
   void addTodo() {
     setState(() {
@@ -62,7 +94,7 @@ class _HomeScreenState extends State<HomeScreen> {
               child: ListView.builder(
                 itemBuilder: (context, index) {
                   return ListTile(
-                    title: Text(todoList[index]),
+                    title: Text(todoList[index]["title"]),
                     trailing: IconButton(
                       icon: const Icon(Icons.delete),
                       onPressed: () => deleteTodo(index),
